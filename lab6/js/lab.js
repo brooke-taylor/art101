2b2ae5fadeb5b9a77070f95cafec62cfae942c64
volvox8534 <brooke.taylor805@gmail.com>
2024-04-25 17:18:36 -0700

    Lab 6 Update
    Finished lab 6 + updated homepage accordingly !

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
 
-// Constants
+// index.js - Lab 6 - Arrays and Objects Script [Transportation]
+  // Author: Brooke Taylor
+  // Date: April 25
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+  //Declaring Variables in an Array
+  let myTransport = [" Bus", " Car", " Bike"];
+  
+  //Defining Main Method of Transport in an Object
+  let myMainRide = {
+    make: "Bus",
+    model: "Metro",
+    color: "Varies",
+    year: 2000, //an estimate, this also varies.
+    ownIt: false,
+    age : function() {
+      return 2024 - this.year;
+    }
+  }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//Outputting Results
+  document.writeln("Here are kinds of transportation I use: ", myTransport, "</br>");
+  
+  document.writeln("Here are the details of my Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
-// let's get this party started
-main();
+
